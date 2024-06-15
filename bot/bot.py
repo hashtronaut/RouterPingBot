4156284c315e02f053ccdbf34b74d29c4ef1cc45
hashtroaut <astrofixed@om-webmasters.org>
2024-06-15 13:49:08 +0300

    small fix

--- bot/bot.py
+++ bot/bot.py
@@ -9,7 +9,7 @@ logger = logging.getLogger('RouterPingBot')
 
 admin_id = os.environ.get("ADMIN_ID")
 bot_token = os.environ.get("BOT_TOKEN")
-api_id = os.environ.get("API_ID")
+api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 
 
